c340bb295dc125cf3591949615d7639f41768aa8
Themydee <nifetemiboy@gmail.com>
2025-05-24 14:20:40 +0100

    products api

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,98 @@
+import { Product } from '../models/product.models.js';
+import {v2 as cloudinary} from 'cloudinary';
+
+
+export const createProduct = async (req, res) => {
+    try{
+        const { name, description, price, category, subCategory, bestSeller,sizes } = req.body;
+
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+
+        let imageUrls = await Promise.all(
+            images.map(async(item) => {
+                let result = await cloudinary.uploader.upload(item.path, {
+                    resource_type: 'image'
+                });
+                return result.secure_url;
+            })
+        )
+
+        const productData = {
+            name, 
+            description,
+            price: Number(price),
+            category,
+            subCategory,
+            bestSeller: bestSeller === 'true' ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imageUrls,
+            date: Date.now()
+        }
+
+        console.log(productData);
+
+        const product = new Product(productData);
+        await product.save();
+
+         res.status(201).json({
+            success: true,
+            message: "Product created successfully",
+           
+        })
+        
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const listProduct = async (req, res) => {
+    try {
+        const product = await Product.find({})
+        return res.status(200).json({
+            success: true,
+            message: "Product list fetched successfully",
+            product
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.body.id);  
+        return res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+        }) 
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const singleProduct = async (req,res) => {
+    try{
+        const {productId} = req.body;
+        const product = await Product.findById(productId);
+        if (!product) {
+            return res.status(404).json({ success: false, message: "Product not found" });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Product fetched successfully",
+            product
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
---
+++ server/routes/product.route.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import upload from '../middleware/multer.middleware.js'
+import { createProduct, listProduct, deleteProduct, singleProduct } from '../controllers/product.controller.js'
+
+
+const router = express.Router() 
+
+router.post('/create', 
+    upload.fields([
+        {name: 'image1', maxCount:1}, 
+        {name: 'image2', maxCount:1}, 
+        {name: 'image3', maxCount:1}, 
+        {name: 'image4', maxCount:1}
+    ]), 
+createProduct)
+
+router.get('/list', listProduct)
+
+router.post('/delete', deleteProduct)
+
+router.post('/single', singleProduct)
+
+export default router;
